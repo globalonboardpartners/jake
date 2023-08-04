b3b1d794e90f18c2778070c8f642e841cdfe6288
HarrisonHemstreet <HarrisonHemstreet@gmail.com>
2023-08-04 15:15:13 -0600

    add tags table and sql constraints

--- exploro.sql
+++ exploro.sql
@@ -12,8 +12,14 @@ CREATE TABLE continent (
   special_offer_image_link,
   video_link TEXT,
   gallery TEXT[],
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+);
+
+ALTER TABLE continent
+ADD CONSTRAINT fk_continent_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 CREATE TABLE country (
   id SERIAL PRIMARY KEY,
@@ -26,8 +32,14 @@ CREATE TABLE country (
   special_offer_image_link TEXT,
   video_link TEXT,
   gallery TEXT[],
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE country
+ADD CONSTRAINT fk_country_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 CREATE TABLE region (
   id SERIAL PRIMARY KEY,
@@ -40,8 +52,14 @@ CREATE TABLE region (
   special_offer_image_link TEXT,
   video_link TEXT,
   gallery TEXT[],
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE region
+ADD CONSTRAINT fk_region_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 CREATE TABLE city (
   id SERIAL PRIMARY KEY,
@@ -55,8 +73,14 @@ CREATE TABLE city (
   video_link TEXT,
   gallery TEXT[],
   featured_city BOOLEAN,
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE city
+ADD CONSTRAINT fk_city_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 -- formally known as "business"
 CREATE TABLE partner_vendor (
@@ -81,8 +105,14 @@ CREATE TABLE partner_vendor (
   email TEXT,
   phone VARCHAR(16),
   website_link TEXT
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE partner_vendor
+ADD CONSTRAINT fk_partner_vendor_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 -- I want to change the name of the "collection_id" and "item_id" to be something significantly more descriptive
 -- restaurant table has significant differences from the csv joe gave me
@@ -108,8 +138,14 @@ CREATE TABLE restaurant (
   email TEXT,
   phone VARCHAR(16),
   website_link TEXT
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE restaurant
+ADD CONSTRAINT fk_restaurant_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 CREATE TABLE hotel (
   id SERIAL PRIMARY KEY,
@@ -133,8 +169,14 @@ CREATE TABLE hotel (
   email TEXT,
   phone VARCHAR(16),
   website_link TEXT
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE hotel
+ADD CONSTRAINT fk_hotel_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 CREATE TABLE activity (
   id SERIAL PRIMARY KEY,
@@ -158,8 +200,14 @@ CREATE TABLE activity (
   email TEXT,
   phone VARCHAR(16),
   website_link TEXT
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE activity
+ADD CONSTRAINT fk_activity_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 CREATE TABLE event (
   id SERIAL PRIMARY KEY,
@@ -183,8 +231,14 @@ CREATE TABLE event (
   email TEXT,
   phone VARCHAR(16),
   website_link TEXT
+  tags integer[],
   created TIMESTAMP DEFAULT NOW(),
 )
+
+ALTER TABLE event
+ADD CONSTRAINT fk_event_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
 
 CREATE TABLE event_details (
   id SERIAL PRIMARY KEY,
@@ -201,5 +255,16 @@ CREATE TABLE event_details (
   region refrences region(id),
   ticket_link TEXT
   gallery TEXT[],
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
+)
+
+ALTER TABLE event_details
+ADD CONSTRAINT fk_event_details_references_tag
+FOREIGN KEY (tags)
+REFERENCES tag (id);
+
+CREATE TABLE tag (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(20),
 )
