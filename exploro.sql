df5b10f01b3f8001b6bc534687fa2d2c4cae65c3
HarrisonHemstreet <HarrisonHemstreet@gmail.com>
2023-08-04 20:24:57 -0600

    fix exploro.sql

--- exploro.sql
+++ exploro.sql
@@ -1,5 +1,13 @@
 CREATE database exploro;
 
+CREATE OR REPLACE FUNCTION update_updated_on()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.edited = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 -- formally known as "Region"
 CREATE TABLE continent (
   id SERIAL PRIMARY KEY,
@@ -9,17 +17,18 @@ CREATE TABLE continent (
   description_short varchar(200),
   image_link TEXT,
   thumbnail_link TEXT,
-  special_offer_image_link,
+  special_offer_image_link TEXT,
   video_link TEXT,
   gallery TEXT[],
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
+  edited TIMESTAMP DEFAULT NOW()
 );
 
-ALTER TABLE continent
-ADD CONSTRAINT fk_continent_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_continent
+BEFORE UPDATE ON continent
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 CREATE TABLE country (
   id SERIAL PRIMARY KEY,
@@ -34,12 +43,13 @@ CREATE TABLE country (
   gallery TEXT[],
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE country
-ADD CONSTRAINT fk_country_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_country
+BEFORE UPDATE ON country
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 CREATE TABLE region (
   id SERIAL PRIMARY KEY,
@@ -54,12 +64,13 @@ CREATE TABLE region (
   gallery TEXT[],
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE region
-ADD CONSTRAINT fk_region_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_region
+BEFORE UPDATE ON country
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 CREATE TABLE city (
   id SERIAL PRIMARY KEY,
@@ -75,12 +86,13 @@ CREATE TABLE city (
   featured_city BOOLEAN,
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE city
-ADD CONSTRAINT fk_city_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_city
+BEFORE UPDATE ON city
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 -- formally known as "business"
 CREATE TABLE partner_vendor (
@@ -94,26 +106,25 @@ CREATE TABLE partner_vendor (
   image_link_2 TEXT,
   thumbnail_link TEXT,
   gallery TEXT[],
-  featured_restaurant BOOLEAN,
-  parter_vender_id refrences parter_vender(id),
-  continent refrences continent(id),
-  country refrences country(id),
-  region refrences region(id),
-  city refrences city(id),
+  featured_partner_vendor BOOLEAN,
+  continent integer references continent(id),
+  country integer references country(id),
+  region integer references region(id),
+  city integer references city(id),
   latatude FLOAT,
   longitude FLOAT,
   email TEXT,
   phone VARCHAR(16),
-  website_link TEXT
+  website_link TEXT,
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE partner_vendor
-ADD CONSTRAINT fk_partner_vendor_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
-
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_partner_vendor
+BEFORE UPDATE ON partner_vendor
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 -- I want to change the name of the "collection_id" and "item_id" to be something significantly more descriptive
 -- restaurant table has significant differences from the csv joe gave me
 CREATE TABLE restaurant (
@@ -127,25 +138,27 @@ CREATE TABLE restaurant (
   image_link_2 TEXT,
   thumbnail_link TEXT,
   gallery TEXT[],
+  menu_gallery TEXT[],
   featured_restaurant BOOLEAN,
-  parter_vender_id refrences parter_vender(id),
-  continent refrences continent(id),
-  country refrences country(id),
-  region refrences region(id),
-  city refrences city(id),
+  partner_vendor_id integer references partner_vendor(id),
+  continent integer references continent(id),
+  country integer references country(id),
+  region integer references region(id),
+  city integer references city(id),
   latatude FLOAT,
   longitude FLOAT,
   email TEXT,
   phone VARCHAR(16),
-  website_link TEXT
+  website_link TEXT,
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE restaurant
-ADD CONSTRAINT fk_restaurant_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_partner_restaurant
+BEFORE UPDATE ON restaurant
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 CREATE TABLE hotel (
   id SERIAL PRIMARY KEY,
@@ -158,25 +171,47 @@ CREATE TABLE hotel (
   image_link_2 TEXT,
   thumbnail_link TEXT,
   gallery TEXT[],
-  featured_restaurant BOOLEAN,
-  parter_vender_id refrences parter_vender(id),
-  continent refrences continent(id),
-  country refrences country(id),
-  region refrences region(id),
-  city refrences city(id),
+  featured_hotel BOOLEAN,
+  partner_vendor_id integer references partner_vendor(id),
+  continent integer references continent(id),
+  country integer references country(id),
+  region integer references region(id),
+  city integer references city(id),
   latatude FLOAT,
   longitude FLOAT,
   email TEXT,
   phone VARCHAR(16),
-  website_link TEXT
+  website_link TEXT,
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE hotel
-ADD CONSTRAINT fk_hotel_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_partner_hotel
+BEFORE UPDATE ON hotel
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
+
+CREATE TABLE hotel_room (
+  id SERIAL PRIMARY KEY,
+  name varchar(50),
+  hotel_id integer references hotel(id),
+  description_short varchar(100),
+  description_long varchar(200),
+  video_link TEXT,
+  image_link TEXT,
+  image_link_2 TEXT,
+  thumbnail_link TEXT,
+  gallery TEXT[],
+  tags TEXT[],
+  created TIMESTAMP DEFAULT NOW(),
+  edited TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_partner_hotel_room
+BEFORE UPDATE ON hotel_room
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 CREATE TABLE activity (
   id SERIAL PRIMARY KEY,
@@ -189,25 +224,26 @@ CREATE TABLE activity (
   image_link_2 TEXT,
   thumbnail_link TEXT,
   gallery TEXT[],
-  featured_restaurant BOOLEAN,
-  parter_vender_id refrences parter_vender(id),
-  continent refrences continent(id),
-  country refrences country(id),
-  region refrences region(id),
-  city refrences city(id),
+  featured_activity BOOLEAN,
+  partner_vendor_id integer references partner_vendor(id),
+  continent integer references continent(id),
+  country integer references country(id),
+  region integer references region(id),
+  city integer references city(id),
   latatude FLOAT,
   longitude FLOAT,
   email TEXT,
   phone VARCHAR(16),
-  website_link TEXT
+  website_link TEXT,
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE activity
-ADD CONSTRAINT fk_activity_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_activity
+BEFORE UPDATE ON activity
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 CREATE TABLE event (
   id SERIAL PRIMARY KEY,
@@ -220,25 +256,27 @@ CREATE TABLE event (
   image_link_2 TEXT,
   thumbnail_link TEXT,
   gallery TEXT[],
-  featured_restaurant BOOLEAN,
-  parter_vender_id refrences parter_vender(id),
-  continent refrences continent(id),
-  country refrences country(id),
-  region refrences region(id),
-  city refrences city(id),
+  featured_event BOOLEAN,
+  ticket_link TEXT,
+  partner_vendor_id integer references partner_vendor(id),
+  continent integer references continent(id),
+  country integer references country(id),
+  region integer references region(id),
+  city integer references city(id),
   latatude FLOAT,
   longitude FLOAT,
   email TEXT,
   phone VARCHAR(16),
-  website_link TEXT
-  tags integer[],
+  website_link TEXT,
+  tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE event
-ADD CONSTRAINT fk_event_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_event
+BEFORE UPDATE ON event
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
 
 CREATE TABLE event_details (
   id SERIAL PRIMARY KEY,
@@ -250,21 +288,17 @@ CREATE TABLE event_details (
   event_artist_slug VARCHAR(100),
   venue_name VARCHAR(100),
   event_time TIME,
-  city refrences city(id),
-  country refrences country(id),
-  region refrences region(id),
-  ticket_link TEXT
+  city integer references city(id),
+  country integer references country(id),
+  region integer references region(id),
+  ticket_link TEXT,
   gallery TEXT[],
   tags TEXT[],
   created TIMESTAMP DEFAULT NOW(),
-)
+  edited TIMESTAMP DEFAULT NOW()
+);
 
-ALTER TABLE event_details
-ADD CONSTRAINT fk_event_details_references_tag
-FOREIGN KEY (tags)
-REFERENCES tag (id);
-
-CREATE TABLE tag (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(20),
-)
+CREATE TRIGGER trigger_row_edit_update_timestamp_for_event_details
+BEFORE UPDATE ON event_details
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_on();
