18ee0b7f54e730643cf1325208fc24d384457ddf
HarrisonHemstreet <HarrisonHemstreet@gmail.com>
2023-09-26 18:13:11 -0600

    add sql updates

--- sql/city.sql
+++ sql/city.sql
@@ -20,19 +20,6 @@ BEFORE UPDATE ON city
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_on();
 
--- -- Insert for City
--- INSERT INTO city (name, slug, description_long, description_short, image_link, thumbnail_link, featured_city, gallery, created, edited)
--- VALUES ('Dubai', 'dubai', 'Long description for Dubai', 'Short description for Dubai', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('Amsterdam', 'amsterdam', 'Long description for Amsterdam', 'Short description for Amsterdam', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('Paris', 'paris', 'Long description for Paris', 'Short description for Paris', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('Cape Town', 'cape-town', 'Long description for Cape Town', 'Short description for Cape Town', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('Aberdeen', 'aberdeen', 'Long description for Aberdeen', 'Short description for Aberdeen', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('New York City', 'new-york-city', 'Long description for New York City', 'Short description for New York City', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('Darwin', 'darwin', 'Long description for Darwin', 'Short description for Darwin', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('Melbourne', 'melbourne', 'Long description for Melbourne', 'Short description for Melbourne', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('Delhi', 'delhi', 'Long description for Delhi', 'Short description for Delhi', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
---        ('London', 'london', 'Long description for London', 'Short description for London', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
 -- Insert for City
 INSERT INTO city (name, slug, description_long, description_short, image_link, thumbnail_link, featured_city, gallery, created, edited)
 VALUES ('MISSING DATA', 'missing-data', 'Long description for MISSING DATA', 'Short description for MISSING DATA', 'image_link_here', 'thumbnail_link_here', 0.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
@@ -45,4 +32,15 @@ VALUES ('MISSING DATA', 'missing-data', 'Long description for MISSING DATA', 'Sh
        ('Darwin', 'darwin', 'Long description for Darwin', 'Short description for Darwin', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('Melbourne', 'melbourne', 'Long description for Melbourne', 'Short description for Melbourne', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('Delhi', 'delhi', 'Long description for Delhi', 'Short description for Delhi', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('London', 'london', 'Long description for London', 'Short description for London', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+       ('London', 'london', 'Long description for London', 'Short description for London', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Penang', 'penang', 'Long description for Penang', 'Short description for Penang', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Kuala Lumpur', 'kuala-lumpur', 'Long description for Kuala Lumpur', 'Short description for Kuala Lumpur', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Hamburg', 'hamburg', 'Long description for Hamburg', 'Short description for Hamburg', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Salt Lake City', 'salt-lake-city-a', 'Long description for Salt Lake City', 'Short description for Salt Lake City', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Ho Chi Minh City', 'ho-chi-minh-city', 'Long description for Ho Chi Minh City', 'Short description for Ho Chi Minh City', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Kota Bharu', 'kota-bharu', 'Long description for Kota Bharu', 'Short description for Kota Bharu', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Kota Kinabalu', 'kota-kinabalu', 'Long description for Kota Kinabalu', 'Short description for Kota Kinabalu', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Bangkok', 'bangkok-a', 'Long description for Bangkok', 'Short description for Bangkok', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Kuching', 'kuching', 'Long description for Kuching', 'Short description for Kuching', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Miri', 'miri', 'Long description for Miri', 'Short description for Miri', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Oslo', 'oslo', 'Long description for Oslo', 'Short description for Oslo', 'image_link_here', 'thumbnail_link_here', 1.0, ARRAY['gallery1', 'gallery2'], CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
