10d7c46b50c5ebdfc169ad48123f48410b4e34f8
HarrisonHemstreet <HarrisonHemstreet@gmail.com>
2023-10-03 14:13:10 -0600

    get_db_url.sh

--- get_db_url.sh
+++ get_db_url.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-interface=$(ip route | grep default | awk '{print $5}')
-ip_address=$(ip addr show $interface | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+
+# Detect the operating system
+os=$(uname)
+
+# Get the network interface and IP address based on the OS
+if [ "$os" == "Linux" ]; then
+  interface=$(ip route | grep default | awk '{print $5}')
+  ip_address=$(ip addr show $interface | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+elif [ "$os" == "Darwin" ]; then  # macOS
+  interface=$(route -n get default | grep 'interface:' | awk '{print $2}')
+  ip_address=$(ifconfig $interface | grep 'inet ' | awk '{print $2}')
+else
+  echo "Unsupported OS"
+  exit 1
+fi
 
 # Copy the example env file to .env if it doesn't exist
 [ ! -f .env ] && cp env.example .env
 
 # Remove the existing DATABASE_URL line
-sed -i '/^DATABASE_URL/d' .env
-
-# Append the new DATABASE_URL line
-echo "DATABASE_URL=postgres://root:root@$ip_address:5440/exploro" >> .env
+if [ "$os" == "Linux" ]; then
+  sed -i '/^DATABASE_URL/d' .env
+elif [ "$os" == "Darwin" ]; then
+  sed -i '' '/^DATABASE_URL/d' .env
+fi
